e14397215f6c2dffb26ac2c9942fd1ddb5d8bdb9
Guilherme Fernando Angelico <guilherme.angelico@s2it.com.br>
2018-02-14 16:59:19 -0200

    Inclusao de projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+import { ROUTES } from './app.route';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { RestaurantComponent } from './restaurantes/restaurant/restaurant.component';
+import { RestaurantService } from './restaurantes/restaurant.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantesComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [RestaurantService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
