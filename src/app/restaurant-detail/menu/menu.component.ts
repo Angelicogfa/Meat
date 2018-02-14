8301fa5e964e3d4db3e4ddee55c3d82ec9dadb91
Guilherme Fernando Angelico <guilherme.angelico@s2it.com.br>
2018-02-14 17:27:21 -0200

    inclusao

---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html',
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { 
+    //TODO - Alteração
+  }
+
+  ngOnInit() {
+  }
+
+}
