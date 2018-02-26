8020b099c69b5f69ae220b23f324f6ff7ea99a39
Guilherme Fernando Angelico <guilherme.angelico@s2it.com.br>
2018-02-26 20:00:48 -0300

    Inclusao de barra de pesquisa

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantService } from '../core/restaurant.service';
-
+import { trigger, state, style, transition, animate } from "@angular/animations";
 
 @Component({
   selector: 'mt-restaurantes',
-  templateUrl: './restaurantes.component.html'
+  templateUrl: './restaurantes.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px",
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition("* => *", animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantesComponent implements OnInit {
 
   restaurants: Restaurant[];
+  searchBarState: string = 'hidden';
 
   constructor(private restaurantService: RestaurantService) {
 
@@ -18,5 +33,9 @@ export class RestaurantesComponent implements OnInit {
   ngOnInit() {
     this.restaurantService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 }
